9e9442ef7f8f9d13a09be07bb1465bd5d49c0311
league <asherwelsbie@icloud.com>
2024-04-06 11:29:49 -0700

    working on netflix

---
+++ src/_04_netflix/Netfilx.java
@@ -0,0 +1,11 @@
+package _04_netflix;
+
+public class Netfilx {
+public static void main(String[] args) {
+	Movie starWars = new Movie("star wars original trilogy", 5);
+	Movie warStars = new Movie("star wars sequel trilogy", 1);
+	Movie dune = new Movie("Dune", 5);
+	Movie readyPlayerOne = new Movie("ready player one", 4);
+	Movie starsOfWar = new Movie("star wars prequel trilogy",4);
+}
+}
